8e8ccd3d6a630c2bde7440adafb67ecc3a506ecd
Chris Perkins <chrisperkinsuk@gmail.com>
2020-11-21 09:37:18 +0100

    Added script for basic-setup.sh. Tested on RPI 3 B+ ubuntu-server 18:04

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,25 +1,75 @@
 #!/bin/bash
 
+var unsername="${1}:-";
+
+function create_new_user() {
+
+  echo "Please enter a name for a new user:"
+
+  read -r username
+
+  echo "This will also create a group called ${username}"
+  echo "and a home directory of /home/${username}/"
+
+  sleep 3
+
+  adduser "${username}"
+}
+
+function make_super_user() {
+  usermod -aG sudo "${username}"
+}
+
+function disable_password_login() {
+
+  if [ -n "${1}" ];
+  then
+    username="${1}";
+  else
+    username="ubuntu";
+  fi
+  
+  echo "Match User ${username}" >> /etc/ssh/sshd_config
+  echo "      PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+  echo "Login disabled for user ${username}."
+
+  sleep 1
+}
+
+function disable_default_login() {
+
+  disable_password_login "ubuntu";
+}
+
 apt-get update
 
 echo "Do you whish to create a new user?"
+echo "Please enter y / n :"
+read -r create_user
 
-read create_user
+echo "Do you want this user to have 'root' user privileges?"
+echo "Please enter y / n :"
+read -r is_root
 
-echo "Please enter a name for a new user:"
+if [[ "${create_user}" == "y" ]] || [[ "${create_user}" == "Y" ]] || 
+   [[ "${create_user}" == "Yes" ]] || [[ "${create_user}" == "yes" ]]
+then
+  create_new_user
+fi
 
-read username
+if [[ "${is_root}" == "y" ]] || [[ "${is_root}" == "Y" ]] ||
+   [[ "${is_root}" == "Yes" ]] || [[ "${is_root}" == "yes" ]]
+then
+  make_super_user
+fi
 
-echo "This will also create a group called '${username}"
-echo "and a home directory of /home/${username}/"
+disable_default_login
 
-sleep 3
+echo "Rebooting! Please be patient and reconnect as your new user in a couple of minutes."
 
-adduser ${username}
-
-usermod -aG sudo ${username}
-
-echo "Match User ubuntu" >> /etc/ssh/sshd_config
-echo "      PasswordAuthentication no" >> /etc/ssh/sshd_config
+sleep 2
 
 reboot
+
+exit 0;
